1b4a4ad77394d9272a68eb4a307d33a2069d3d2b
송태성 <masteage@gmail.com>
2018-09-02 19:52:01 +0900

    [e] init

---
+++ cpp_study/cpp_study/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_study
+//
+//  Created by taeseongsong on 2018. 9. 2..
+//  Copyright © 2018년 taeseongsong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
