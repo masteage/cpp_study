a25bcdf596c539fbb029cb0a4f08e31e5c5de9d7
송태성 <masteage@gmail.com>
2019-01-22 01:27:42 +0900

    no message

--- cpp_study/cpp_study/chapter/ch_21/ch_21.cpp
+++ cpp_study/cpp_study/chapter/ch_21/ch_21.cpp
@@ -154,6 +154,122 @@ void ch_21_2_1(){
 	cout << *psGrid2.getElementAt(1, 0) << endl; // psGrid2 is modified
 }
 
+static const size_t NOT_FOUND = (size_t)(-1);
+
+template <typename T>
+size_t Find(T& value, T* arr, size_t size)
+{
+	cout << "original" << endl;
+	for (size_t i = 0; i < size; i++) {
+		if (arr[i] == value) {
+			return i; // found it; return the index
+		}
+	}
+	return NOT_FOUND; // failed to find it; return NOT_FOUND
+}
+
+template <typename T>
+size_t Find(T*& value, T** arr, size_t size)
+{
+	cout << "ptr special" << endl;
+	for (size_t i = 0; i < size; i++) {
+		if (*arr[i] == *value) {
+			return i; // found it; return the index
+		}
+	}
+	return NOT_FOUND; // failed to find it; return NOT_FOUND
+}
+
+// This does not work.
+//template <typename T>
+//size_t Find<T*>(T*& value, T** arr, size_t size)
+//{
+//	cout << "ptr special" << endl;
+//	for (size_t i = 0; i < size; i++) {
+//		if (*arr[i] == *value) {
+//			return i; // found it; return the index
+//		}
+//	}
+//	return NOT_FOUND; // failed to find it; return NOT_FOUND
+//}
+
+template<>
+size_t Find<const char*>(const char*& value, const char** arr, size_t size)
+{
+	cout << "Specialization" << endl;
+	for (size_t i = 0; i < size; i++) {
+		if (strcmp(arr[i], value) == 0) {
+			return i; // found it; return the index
+		}
+	}
+	return NOT_FOUND; // failed to find it; return NOT_FOUND
+}
+
+size_t Find(const char*& value, const char** arr, size_t size)
+{
+	cout << "overload" << endl;
+	for (size_t i = 0; i < size; i++) {
+		if (strcmp(arr[i], value) == 0) {
+			return i; // found it; return the index
+		}
+	}
+	return NOT_FOUND; // failed to find it; return NOT_FOUND
+}
+
+void ch_21_3(){
+	size_t res = NOT_FOUND;
+	
+	int x = 3, intArr[] = { 1, 2, 3, 4 };
+	size_t sizeArr = sizeof(intArr) / sizeof(int);
+	res = Find(x, intArr, sizeArr);      // calls Find<int> by deduction
+	res = Find<int>(x, intArr, sizeArr); // calls Find<int> explicitly
+	
+	double d1 = 5.6, dArr[] = { 1.2, 3.4, 5.7, 7.5 };
+	sizeArr = sizeof(dArr) / sizeof(double);
+	res = Find(d1, dArr, sizeArr);         // calls Find<double> by deduction
+	res = Find<double>(d1, dArr, sizeArr); // calls Find<double> explicitly
+	
+	const char* word = "two";
+	const char* arr[] = { "one", "two", "three", "four" };
+	sizeArr = sizeof(arr) / sizeof(arr[0]);
+	res = Find<const char*>(word, arr, sizeArr);  // calls template specialization for const char*s
+	res = Find(word, arr, sizeArr);         // calls overloaded Find for const char*s
+	
+	int *px = &x, *pArr[] = { &x, &x };
+	sizeArr = sizeof(pArr) / sizeof(pArr[0]);
+	res = Find(px, pArr, sizeArr);    // calls the overloaded Find for pointers
+	
+	SpreadsheetCell c1(10), c2[] = { SpreadsheetCell(4), SpreadsheetCell(10) };
+	sizeArr = sizeof(c2) / sizeof(c2[0]);
+	res = Find(c1, c2, sizeArr);                  // calls Find<SpreadsheetCell> by deduction
+	res = Find<SpreadsheetCell>(c1, c2, sizeArr); // calls Find<SpreadsheetCell> explicitly
+	
+	SpreadsheetCell *pc1 = &c1;
+	SpreadsheetCell *psa[] = { &c1, &c1 };
+	sizeArr = sizeof(psa) / sizeof(psa[0]);
+	res = Find(pc1, psa, sizeArr); // Calls the overloaded Find for pointers
+}
+
+#include "OneDGrid.h"
+void ch_21_4_1(){
+	OneDGrid<int> singleDGrid;
+	OneDGrid<OneDGrid<int>> twoDGrid;
+	OneDGrid<OneDGrid<OneDGrid<int>>> threeDGrid;
+	
+	singleDGrid[3] = 5;
+	twoDGrid[3][3] = 5;
+	threeDGrid[3][3][3] = 5;
+}
+
+#include "NDGrid.h"
+void ch_21_4_2(){
+	NDGrid<int, 3> my3DGrid(3);
+	my3DGrid[2][1][2] = 5;
+	my3DGrid[1][1][1] = 5;
+	
+	cout << my3DGrid[2][1][2] << endl;
+}
+
 void ch_21_main(){
 //	ch_21_1_1();
 //	ch_21_1_1_1();
@@ -161,6 +277,9 @@ void ch_21_main(){
 //	ch_21_1_3();
 //	ch_21_1_3_1();
 //	ch_21_2();
-	ch_21_2_1();
+//	ch_21_2_1();
+//	ch_21_3();
+//	ch_21_4_1();
+	ch_21_4_2();
 	cout << "" << endl;
 }
